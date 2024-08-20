686ccff756548a83214900e86ec175961567a831
Fernando Briano <fernando@picandocodigo.net>
2024-08-20 09:44:18 +0100

    Refactors reporter.
    As the script grew and became more complex, it needed some
    refactoring. I updated it to hopefully become more clear and easy to
    maintain.
    
    The reporter has less responsibilty and each endpoint becomes an object
    which deals with the responsibility of setting up the availability and
    tested values. I fixed some issues on the way, such as the calculation
    for coverage in Stack which was reporting a lower percentage (since it
    was being compared to the JSON spec instead of
    elasticsearch-specification).
    
    I also improved the reading of files, so now the script runs in a
    couple of seconds instead of a few seconds like before (it was not
    optimized at all).

---
+++ report/lib/api_endpoint.rb
@@ -0,0 +1,82 @@
+module Elastic
+  class ApiEndpoint
+    TESTS_PATH = File.expand_path('../tests/**/*.yml')
+
+    attr_reader :name, :available_stack, :available_serverless
+
+    def initialize(spec)
+      @name = spec['name']
+      @availability = spec['availability']
+
+      @test_stack = find_tested(:stack) if available_stack?
+      @test_serverless = find_tested(:serverless) if available_serverless?
+    end
+
+    # The absence of an 'availability' field on a property implies that the property is
+    # available in all flavors.
+    def available_stack?
+      @available_stack ||= @availability.nil? || @availability.dig('stack', 'visibility') != 'private'
+    end
+
+    def available_serverless?
+      @available_serverless ||= @availability.nil? || (
+        @availability['serverless'] &&
+        @availability.dig('serverless', 'visibility') == 'public'
+      )
+    end
+
+    def tested_stack?
+      !@test_stack.nil? && !@test_stack.empty?
+    end
+
+    def tested_serverless?
+      !@test_serverless.nil? && !@test_serverless.empty?
+    end
+
+    def display_tested_stack
+      if @test_stack && @test_stack[:file]
+        "[✅](#{@test_stack[:file]}\#L#{@test_stack[:line]})</li></ul>"
+      elsif available_stack?
+        '❌'
+      else
+        'Not Applicable'
+      end
+    end
+
+    def display_tested_serverless
+      if @test_serverless && @test_serverless[:file]
+        "[✅](#{@test_serverless[:file]}\#L#{@test_serverless[:line]})</li></ul>"
+      elsif available_serverless?
+        '❌'
+      else
+        'Not Applicable'
+      end
+    end
+
+    private
+
+    # For a given flavour (:stack or :serverless), find if there are any tests that call this endpoint.
+    def find_tested(flavour)
+      Dir[TESTS_PATH].map do |path|
+        relative_path = path[path.index('/tests')..]
+        file_content = File.read(path)
+        # Move along if these are not the tests we're looking for
+        next unless file_content.include?("#{flavour}: true") && file_content.include?(@name)
+
+        file_content.split("\n").each_with_index do |line, index|
+          next if line.empty?
+
+          api_mention = line.split(':')[0].strip.gsub('"', '')
+          next unless api_mention == @name
+          next unless Regexp.new(/^#{api_mention}/) =~ @name
+
+          return {
+            file: ".#{relative_path}",
+            line: index + 1
+          }
+        end
+      end
+      {}
+    end
+  end
+end
--- report/reporter.rb
+++ report/reporter.rb
@@ -1,3 +1,5 @@
+require_relative 'lib/api_endpoint'
+
 module Elastic
   # Generates API report.
   #
@@ -16,155 +18,55 @@ module Elastic
       { name: 'rollup', reason: 'The rollup feature was never GA-ed and is still tech preview. It has been deprecated since 8.11.0 in favor of downsampling.' }
     ].freeze
 
-    attr_reader :apis, :stack_tested_count, :serverless_tested_count, :stack_untested_count, :serverless_untested_count
+    attr_reader :endpoints, :internal
 
     def initialize
-      @apis = {}
-      @apis[:internal] = Set[]
-      @apis[:specification] = []
-      @apis[:json] = []
-      @stack_tested_count = 0
-      @stack_untested_count = 0
-      @serverless_tested_count = 0
-      @serverless_untested_count = 0
-      report!
-    end
+      @endpoints = []
+      @internal = []
 
-    # Stack APIs are obtained from the Elasticsearch Rest JSON specification.
-    # Use `rake download_stack` to download the spec files to ../tmp.
-    #
-    def build_json_apis!
-      @apis[:json] = []
-      apis = Dir[STACK_FILES].map { |path| path.split('/').last.gsub('.json', '') }
-      apis.each do |name|
-        if name.start_with?('_')
-          @apis[:internal] << { name: name, reason: 'Internal API' }
-        elsif (skippable = EXCLUDED_APIS.select { |api| name.match? api[:name] }.first)
-          @apis[:internal] << skippable
-        else
-          @apis[:json] << name
-        end
-      end
+      setup
     end
 
     # Serverless APIs are obtained from elastic/elasticsearch-specification.
     # Use `rake download_serverless` to download the files to ../tmp.
-    def build_specification_apis!
-      @apis[:specification] = []
+    def setup
       JSON.parse(File.read('./tmp/schema.json'))['endpoints'].map do |spec|
         if spec['name'].start_with?('_')
-          @apis[:internal] << { name: spec['name'], reason: 'Internal API' }
+          @internal << { name: spec['name'], reason: 'Internal API' }
         elsif (skippable = EXCLUDED_APIS.select { |api| spec['name'].match? api[:name] }.first)
-          @apis[:internal] << skippable
+          @internal << skippable
         elsif spec.dig('availability', 'stack', 'visibility') == 'private'
-          @apis[:internal] << { name: spec['name'], reason: 'Private API' }
+          @internal << { name: spec['name'], reason: 'Private API' }
         else
-          @apis[:specification] << { 'name' => spec['name'], 'availability' => spec['availability'] }
-        end
-      end
-    end
-
-    def stack_apis
-      @apis[:specification].select do |api|
-        api['availability'].nil? ||
-          api.dig('availability', 'stack', 'visibility') != 'private'
-      end
-    end
-
-    def serverless_apis
-      # The absence of an 'availability' field on a property implies that the property is
-      # available in all flavors.
-      @apis[:specification].select do |api|
-        api['availability'].nil? ||
-          (
-            api.dig('availability', 'serverless') &&
-            api.dig('availability', 'serverless', 'visibility') == 'public'
-          )
-      end
-    end
-
-    def report!
-      build_specification_apis!
-      build_json_apis!
-      calculate_tested_untested!
-    end
-
-    def calculate_tested_untested!
-      serverless_apis.each do |api|
-        if find_test(api['name'], :serverless)
-          @serverless_tested_count += 1
-        else
-          @serverless_untested_count += 1
-        end
-      end
-
-      stack_apis.each do |api|
-        if find_test(api['name'], :stack)
-          @stack_tested_count += 1
-        else
-          @stack_untested_count += 1
+          @endpoints << ApiEndpoint.new(spec)
         end
       end
     end
 
     def coverage_serverless
-      @serverless_tested_count * 100 / serverless_apis.count
+      @endpoints.count(&:tested_serverless?) * 100 / @endpoints.count(&:available_serverless?)
     end
 
     def coverage_stack
-      @stack_tested_count * 100 / @apis[:json].count
+      @endpoints.count(&:tested_stack?) * 100 / @endpoints.count(&:available_stack?)
+    end
+
+    def untested_stack_count
+      @endpoints.count { |api| api.available_stack? && !api.tested_stack? }
+    end
+
+    def untested_serverless_count
+      @endpoints.count do |api|
+        api.available_serverless? && !api.tested_serverless?
+      end
     end
 
     def display_table
-      @apis[:specification].map do |api|
-        name = api['name']
-        tested_stack = if (test = find_test(name, :stack))
-                         "[✅](#{test[:file]}\#L#{test[:line]})</li></ul>"
-                       elsif stack_apis.include?(api)
-                         '❌'
-                       else
-                         'Not Applicable'
-                       end
-        tested_serverless = if (test = find_test(name, :serverless))
-                              "[✅](#{test[:file]}\#L#{test[:line]})</li></ul>"
-                            elsif serverless_apis.include?(api)
-                              '❌'
-                            else
-                              'Not Applicable'
-                            end
-        "| #{name} | #{stack_apis.include?(api) ? '🟢' : '🔴'} " \
-        "| #{tested_stack} | #{serverless_apis.include?(api) ? '🟢' : '🔴'} | #{tested_serverless} |"
+      @endpoints.map do |endpoint|
+        "| #{endpoint.name} | #{endpoint.available_stack? ? '🟢' : '🔴'} " \
+        "| #{endpoint.display_tested_stack} | #{endpoint.available_serverless? ? '🟢' : '🔴'} " \
+        "| #{endpoint.display_tested_serverless}"
       end.join("\n")
-    end
-
-    private
-
-    def find_test(endpoint, flavour = nil)
-      Dir[TESTS_PATH].map do |path|
-        relative_path = path[path.index('/tests')..]
-        next unless File.read(path).include?("#{flavour}: true")
-
-        File.readlines(path).each_with_index do |line, index|
-          api_mention = line.split(':')[0].strip.gsub('"', '')
-          next unless api_mention == endpoint
-          next unless Regexp.new(/^#{api_mention}/) =~ endpoint
-
-          requires = find_requires(path)
-          return {
-            endpoint: endpoint,
-            file: ".#{relative_path}",
-            line: index + 1,
-            serverless: requires['serverless'],
-            stack: requires['stack']
-          }
-        end
-      end
-      false
-    end
-
-    def find_requires(path)
-      require 'yaml'
-      YAML.load_stream(File.read(path)).select { |a| a.keys.first == 'requires' }.first['requires']
     end
   end
 end
